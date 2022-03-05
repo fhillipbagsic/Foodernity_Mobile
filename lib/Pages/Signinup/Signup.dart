b9dbcd36e740144bdf6b021973d0bea265e5114b
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-03-05 17:07:06 +0800

    Add signup page

---
+++ lib/Pages/Signinup/Signup.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:sizer/sizer.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+
+  var fullNameController = TextEditingController();
+  var emailAddressController = TextEditingController();
+  var passwordController = TextEditingController();
+  var confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        color: Colors.blue,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.h),
+              child: Text(
+                'Sign up to Foodernity',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.sp),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 7.w, vertical: 5.h),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20.0),
+                    topLeft: Radius.circular(20.0),
+                  ),
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      _field('Full Name', fullNameController),
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      _field('Email Address', emailAddressController),
+                      _field('Password', passwordController),
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      _confirmPasswordField(),
+                      _agreement(),
+                      SizedBox(
+                        height: 3.h,
+                      ),
+                      _signupButton()
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget _field(
+    String fieldName,
+    TextEditingController controller,
+  ) {
+    return TextFormField(
+      controller: controller,
+      // onSaved: (value) => controller.value = value,
+      validator: fieldName == 'Full Name'
+          ? fullNameValidator
+          : fieldName == 'Email Address'
+              ? emailValidator
+              : passwordValidator,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: fieldName,
+        helperText: getHelperText(fieldName),
+        suffixIcon: IconButton(
+          onPressed: () => controller.clear(),
+          icon: const Icon(
+            Icons.clear,
+            size: 15.0,
+          ),
+        ),
+      ),
+      obscureText: fieldName == 'Password' ? true : false,
+    );
+  }
+
+  Widget _confirmPasswordField() {
+    return TextFormField(
+      controller: confirmPasswordController,
+      validator: (val) => MatchValidator(errorText: 'Passwords do not match')
+          .validateMatch(
+              confirmPasswordController.text, passwordController.text),
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: 'Confirm Password',
+        helperText: getHelperText('Confirm Password'),
+        suffixIcon: IconButton(
+          onPressed: () => confirmPasswordController.clear(),
+          icon: const Icon(
+            Icons.clear,
+            size: 15.0,
+          ),
+        ),
+      ),
+      obscureText: true,
+    );
+  }
+
+  final fullNameValidator = MultiValidator([
+    RequiredValidator(errorText: 'Full name is required'),
+    PatternValidator(r'^[a-zA-Z ]+$', errorText: 'Only letters are allowed'),
+    MinLengthValidator(3, errorText: 'Please enter your full name')
+  ]);
+
+  final emailValidator = MultiValidator([
+    RequiredValidator(errorText: "Email address is required"),
+    EmailValidator(errorText: "Enter valid email address"),
+  ]);
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    PatternValidator(r'^[a-zA-Z0-9]+$',
+        errorText: 'passwords must not have special character')
+  ]);
+
+  String getHelperText(String fieldName) {
+    switch (fieldName) {
+      case 'Full Name':
+        return 'First Name, Last Name';
+      case 'Password':
+        return 'Must be at least 8 characters long';
+    }
+    return '';
+  }
+
+  Widget _agreement() {
+    return (RichText(
+      textAlign: TextAlign.center,
+      text: const TextSpan(
+          text:
+              'By signing up for an account, you agree that you have read and accepted our ',
+          style: TextStyle(color: Colors.grey),
+          children: [
+            TextSpan(
+              text: 'Data Privacy Policy.',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          ]),
+    ));
+  }
+
+  Widget _signupButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text('Signing up')));
+          }
+        },
+        child: const Text('Sign up'),
+      ),
+    );
+  }
+}
